09e7d8185bde226d7ad190a423cd82c0c4f1364a
Erika Sanchez <erika.walker.business@gmail.com>
2021-06-13 22:34:20 -0400

    Added personal picture. Edited orientation of content. Changed background color. Added more styling for responsiveness. Hoping this works.

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 // alert("Script loaded")
-
-const img = document.getElementsById("preview");
-
-function openImage() {
-    // img.style.display = "block";
-    img.style.width = "60%";
-    img.style.height = "auto";
-    img.style.transition = "width 0.5s ease";
-}
-
-function closeImage() {
-    // img.style.display = "none";
-    img.style.width = "40%";
-    img.style.height = "auto";
-    img.style.transition = "width 0.5s ease";
-}
